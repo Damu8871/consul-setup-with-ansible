23d1c27175162d68f9449c63db95e588d2642105
Damodaran R <damodaran_r@outlook.com>
2018-06-27 22:44:49 +0530

    Add files via upload

---
+++ shell/install-consul-ui.sh
@@ -0,0 +1,23 @@
+cd ~
+mkdir consul-ui
+cd consul-ui
+sudo wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_web_ui.zip
+sudo unzip consul_0.6.4_web_ui.zip
+sudo rm consul_0.6.4_web_ui.zip
+sudo yum -y install httpd
+mkdir /home/ec2-user/services
+cd /home/ec2-user/services
+sudo cat > web.json <<- "EOF"
+{
+    "services":  {
+        "name": "apache web server",
+        "port": 80,
+        "tags": ["apache", "demonstration"],
+        "check": {
+            "script": "curl localhost:80 > /dev/null 2>&1",
+            "interval": "10s"
+        }
+    }
+}
+EOF
+sudo service httpd restart
