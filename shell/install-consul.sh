f4e952a071e02b18e7f6f182ad0171898cca3851
Damodaran R <damodaran_r@outlook.com>
2018-06-27 22:42:35 +0530

    Create install-consul.sh

---
+++ shell/install-consul.sh
@@ -0,0 +1,6 @@
+cd /usr/local/bin
+sudo yum -y install unzip
+sudo yum -y install wget
+sudo wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
+sudo unzip consul_0.6.4_linux_amd64.zip
+sudo rm consul_0.6.4_linux_amd64.zip
